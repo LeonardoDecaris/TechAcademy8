468569c6461f48ac6d6da9eb3094dfb3d8871e74
lucas-pedrozo <lucaspedroozoo@hotmail.com>
2025-11-19 16:20:55 -0300

    teste comflito

--- PainelAdmin/src/pages/auth/Home.tsx
+++ PainelAdmin/src/pages/auth/Home.tsx
@@ -4,7 +4,8 @@ function Home() {
 
     return(
         <main className="p-4" >
-
+            <h1 className="text-2xl font-bold mb-4">Bem-vindo ao Painel Administrativo</h1>
+            <p>Use o menu lateral para navegar pelas diferentes seções do painel.</p>
         </main>
     );
 }
